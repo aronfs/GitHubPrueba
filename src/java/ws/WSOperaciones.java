8db641fdd2877d93a3f75089b60d0aaf5324450d
Aronfs <aronviracocha7@gmail.com>
2022-07-20 16:36:03 -0500

    Hola mensaje de commit

---
+++ src/java/ws/WSOperaciones.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+
+/**
+ *
+ * @author arons
+ */
+@WebService(serviceName = "WSOperaciones")
+public class WSOperaciones {
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "login")
+    public Boolean login(@WebParam(name = "usuario") String usuario, @WebParam(name = "contrasena") String contrasena) {
+        
+        if (usuario.equals("Aaron") && contrasena.equals("1234")){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "ProcesarPago")
+    public int ProcesarPago(@WebParam(name = "total") int total, @WebParam(name = "pago") int pago) {
+        //TODO write your implementation code here:
+        if(pago>=total){
+           //retornar vuelto
+            return pago-total;
+        }else{
+            return -1;
+        }
+    }
+
+    
+ 
+}
